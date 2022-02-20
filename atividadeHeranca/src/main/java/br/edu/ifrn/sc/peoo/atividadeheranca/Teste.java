573238614bceaefc9742b6d3720f8cc8c9fd6407
tiago-rodrigues1 <tiagords127@gmail.com>
2022-02-20 16:24:14 -0300

    responde questoes 6 e 7

--- atividadeHeranca/src/main/java/br/edu/ifrn/sc/peoo/atividadeheranca/Teste.java
+++ atividadeHeranca/src/main/java/br/edu/ifrn/sc/peoo/atividadeheranca/Teste.java
@@ -1,3 +1,13 @@
+/*
+* Explicação do passo 6:
+* Ao digitarmos p.setCPF(); e p.setCNPJ(); obtemos erro, pois estes métodos não
+* pertencem à classe pessoa. Mas sim à PessoFisica e PessoaJuridica, respectiva-
+* mente.
+*
+* Explicação do passo 7:
+* Ambas as classes imprimiram seus respectivos nomes, duas vezes.
+*/
+
 package br.edu.ifrn.sc.peoo.atividadeheranca;
 
 public class Teste {
